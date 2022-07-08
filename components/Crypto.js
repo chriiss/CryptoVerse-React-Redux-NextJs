d993f66358b2f9395bfff7173208225126d0d12c
chriiss <christopher.kumla@gmail.com>
2022-07-08 21:32:11 +0200

    Add files via upload

---
+++ components/Crypto.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addCryptos } from '/store/Slice';
+import  cryptoData  from '../pages/api/CryptoApi';
+import CryptoList from './CryptoList';
+import FilterList from './FilterList';
+import SearchBox from './SearchBox';
+
+
+const Crypto = () => {
+    const dispatch = useDispatch();
+
+    const getCryptoApi = async () => {
+        await cryptoData
+        .then(result => {
+            dispatch(addCryptos(result.data));
+        });
+    }
+
+    useEffect(() => {
+        getCryptoApi();
+    })
+
+
+
+    return(
+        <div>
+            <SearchBox />
+            <FilterList />
+            <CryptoList/>
+        </div>
+    )
+}
+
+export default Crypto;
