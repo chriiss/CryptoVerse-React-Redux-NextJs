7a1caa73a53f9ad2b536fb48884d1b922467231b
chriiss <christopher.kumla@gmail.com>
2022-07-08 23:18:58 +0200

    modif Crypto file

--- components/Crypto.js
+++ components/Crypto.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'React';
 import { useDispatch } from 'react-redux';
 import cryptoData from "../pages/api/CryptoApi";
 import { addCryptos } from "../store/Slice";
-import { SearchBox } from './SearchBox';
-import { FilterList } from './FilterList';
-import { CryptoList } from './CryptoList';
+import SearchBox from './SearchBox';
+import FilterList from './FilterList';
+import CryptoList from './CryptoList';
 
 
 const Crypto = () => {
@@ -16,9 +16,6 @@ const Crypto = () => {
         .then(result => dispatch(addCryptos(result.data)))
     }
 
-    useEffect(() => {
-        getCryptoData();
-    })
 
     return (
         <div>
