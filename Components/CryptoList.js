b3d58d18630b071c7642430f6ba32b446d8a93eb
chriiss <christopher.kumla@gmail.com>
2022-07-08 21:25:47 +0200

    Add files via upload

---
+++ Components/CryptoList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import { getAllCryptos, getSearch } from '../store/Slice';
+import dataJson from "../data/Data.json";
+import Styles from '../styles/Home.module.scss'
+
+const CryptoList = () => {
+    const cryptoApi = useSelector(getAllCryptos);
+    const query = useSelector(getSearch);
+    const cryptoResult = cryptoApi.filter((data)=>
+        data.name.toLowerCase().includes(query.toLowerCase()) ||
+        data.symbol.toLowerCase().includes(query.toLowerCase())
+    )
+
+    return(
+        <div>
+            <div>{cryptoResult.length} {dataJson.result}</div>
+            {
+            cryptoResult.map((data, index) => (
+                <div key={index}>
+                    <Image src={data.image} width={25} height={25} alt="crypto_icon"/>
+                    {data.market_cap_rank}
+                    {data.name}
+                    {data.symbol}
+                    {dataJson.usd}{data.current_price}
+                    <span className={data.price_change_percentage_24h <= 0 ? Styles.red : Styles.green}>{data.price_change_percentage_24h.toFixed(2)}{dataJson.percent}</span>
+                    {dataJson.usd}{data.market_cap}
+                    {dataJson.usd}{data.total_volume}
+                    {dataJson.usd}{data.ath}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default CryptoList;
---
+++ Components/FilterList.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addCryptos, getAllCryptos } from "../store/Slice";
+import dataJson from "../data/Data.json";
+
+
+const FilterList = () => {
+    const cryptoApi = useSelector(getAllCryptos);
+    const selectCrypto= dataJson.filterCryptos;
+    const dispatch = useDispatch();
+
+    const getSort = (sortBy) => {
+        const sortTypes = {
+            1: (a,b) => a.current_price - b.current_price,
+            2: (a,b) => b.current_price - a.current_price,
+            3: (a,b) => a.market_cap - b.market_cap,
+            4: (a,b) => b.market_cap - a.market_cap,
+            5: (a,b) => a.total_volume - b.total_volume,
+            6: (a,b) => b.total_volume - a.total_volume,
+            7: (a,b) => a.price_change_percentage_24h - b.price_change_percentage_24h,
+            8: (a,b) => b.price_change_percentage_24h - a.price_change_percentage_24h,
+        }
+
+
+        const sortCryptoApi = [...cryptoApi].sort(sortTypes[sortBy]);
+        dispatch(addCryptos(sortCryptoApi));
+    }
+
+    return (
+        <div>
+            {selectCrypto.map((data, index) => {
+                return(
+                    <select key={index} onChange={(e) => getSort(e.target.value)}>
+                        <option value="">{data.label}</option>
+                        <option value={data.value}>{data.name}</option>
+                        <option value={data.value_bis}>{data.name_bis}</option>
+                    </select>
+                )
+            })}
+        </div>
+    )
+}
+
+export default FilterList;
