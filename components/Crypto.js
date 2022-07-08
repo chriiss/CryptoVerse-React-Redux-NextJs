1dc8e847522829882ab7934fd3b6ba2c62a0ec68
chriiss <christopher.kumla@gmail.com>
2022-07-08 22:04:02 +0200

    add Crypto File with code

--- components/Crypto.js
+++ components/Crypto.js
@@ -1,9 +1,33 @@
 import React, { useEffect } from 'React';
+import { useDispatch } from 'react-redux';
+import cryptoData from "../pages/api/CryptoApi";
+import { addCryptos } from "../store/Slice";
+import { SearchBox } from './SearchBox';
+import { FilterList } from './FilterList';
+import { CryptoList } from './CryptoList';
 
 
 const Crypto = () => {
 
-}
+    const dispatch = useDispatch();
 
+    const getCryptoData = async () => {
+        await cryptoData
+        .then(result => dispatch(addCryptos(result.data)))
+    }
+
+    useEffect(() => {
+        getCryptoData();
+    })
+
+    return (
+        <div>
+            <SearchBox />
+            <FilterList />
+            <CryptoList />
+        </div>
+    )
+
+}
 
 export default Crypto;
